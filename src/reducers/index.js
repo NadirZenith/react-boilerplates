c521a6817f12094ab9abdcf08aca557dcca2778b
NadirZenith <2cb.md2@gmail.com>
2017-11-15 19:15:15 +0100

    cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,3 @@ import { combineReducers } from 'redux'
 import { visibilityFilter, todos } from './todo'
 
 export default combineReducers({ visibilityFilter, todos })
-
-
-// export default function(state = initialState, action) {
-//   switch (action.type) {
-//     case actionTypes.ADD_TODO:
-//       return addTodo(state, action);
-//   }
-//   return state;
-// }
-//
-// function addTodo(state, action) {
-//   const { todo } = action;
-//   return [ ...state, ...todo ];
-// }
-
-
-
-// ---------------------
