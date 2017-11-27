e79c2ea1e1a557dce98e120ec92a3d48c9fc116d
NadirZenith <2cb.md2@gmail.com>
2017-11-27 18:48:53 +0100

    uncomment types

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import Todo from './Todo'
 
 const TodoList = ({todos, onTodoClick}) => (
@@ -10,15 +10,15 @@ const TodoList = ({todos, onTodoClick}) => (
     </ul>
 )
 
-// TodoList.propTypes = {
-//     todos: PropTypes.arrayOf(
-//         PropTypes.shape({
-//             id: PropTypes.number.isRequired,
-//             completed: PropTypes.bool.isRequired,
-//             text: PropTypes.string.isRequired
-//         }).isRequired
-//     ).isRequired,
-//     onTodoClick: PropTypes.func.isRequired
-// }
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            completed: PropTypes.bool.isRequired,
+            text: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    onTodoClick: PropTypes.func.isRequired
+}
 
 export default TodoList
